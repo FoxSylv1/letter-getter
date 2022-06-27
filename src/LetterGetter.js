1a5c87c3413c4dc5f8ba9d2f04df46cd4beaa266
FoxSylv <atheriault@bowdoin.edu>
2022-06-27 02:28:56 -0400

    Set up fundamentals.
    Switch the default create-react-app output with a
    personalized variation that better represents my
    plan going foward.

---
+++ src/LetterGetter.js
@@ -0,0 +1,15 @@
+import './LetterGetter.scss';
+
+function LetterGetter() {
+    return (
+        <div id="LetterGetter">
+            <p>
+                This is a work in progress!
+                <br />
+                Please come back later!
+            </p>
+        </div>
+    );
+}
+
+export default LetterGetter;
