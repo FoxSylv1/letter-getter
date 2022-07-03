987accfa88e08cd03bd3eb7062538cb6affdc16b
FoxSylv <atheriault@bowdoin.edu>
2022-07-03 00:37:38 -0400

    Refactor Tile component.
    Remove the double tile-ed nature with a better single-tile approach.

--- src/Tile.js
+++ src/Tile.js
@@ -1,28 +1,18 @@
 import "./Tile.scss"
 
-/* Tile operates by having two "tiles," one for being in a state of on or off.
-The "front" tile is the black on white background and is the starting, unused
-state of the tiles. The "back" tile is the beige on black background and 
-occurs when the tile is hovered over or submitted. */
+function clickTile(tileUsed, submitTile) {
+    if (!tileUsed) {
+        submitTile();
+    }
+}
 
 function Tile(props) {
-    var tileData = [];
-    if (!props.tileUsed) {
-        tileData.push(
-            <button class="unused-tile tile" key={props.id + "-front"} onClick={() => props.submitTile()}>
-                {props.value}
-            </button>
-        );
-    }
-    tileData.push(
-        <div class="used-tile tile" key={props.id + "-back"}>
-            {props.value}
-        </div>
-    );
-
     return (
         <div class="clickable-button tile">
-            {tileData}
+            <button class={"tile " + (props.tileUsed ? "used-tile" : "unused-tile")}
+                    onClick={() => clickTile(props.tileUsed, props.submitTile)}>
+                {props.value}
+            </button>
         </div>
     );
 }
